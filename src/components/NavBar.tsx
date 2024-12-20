a1c33236d31a85ce147a9629eb27700e892742d5
yabetsTemesgen <yabtemesgen@gmail.com>
2024-12-20 16:37:52 +0300

    feat: enhance search functionality and UI elements in Navbar and SearchBar components

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,27 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "./SearchBar";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 const Navbar = () => {
   const pathname = usePathname();
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
 
   if (pathname === "/player") {
     return (
       <nav className="hidden md:flex items-center justify-between bg-black text-white lg:py-10 p-7 h-[64px]">
         <Link href={"/"}><h1 className="text-3xl font-bold">GAMMA</h1></Link>
-        <SearchBar />
+        <SearchBar setIsSearchVisible={setIsSearchVisible} />
       </nav>
     );
   }
   return (
     <nav className="flex items-center justify-between bg-black text-white lg:py-10 p-7 h-[64px]">
-      <Link href={"/"}><h1 className="text-3xl font-bold">GAMMA</h1></Link>
-      <SearchBar />
+      {!isSearchVisible && (
+        <Link href={"/"}><h1 className="text-3xl font-bold">GAMMA</h1></Link>
+      )}
+      <SearchBar setIsSearchVisible={setIsSearchVisible} />
     </nav>
   );
 };
